bd09b54f934767eca76f9d1bfb4f22cbf03a4eb7
Vincent Ambo <tazjin@gmail.com>
2017-02-15 10:29:26 +0100

    refactor: Use Goroutine to read from all shards & simplify

--- main.go
+++ main.go
@@ -15,31 +15,20 @@ var (
 	stream       = flag.String("stream", "your-stream", "your stream name")
 	region       = flag.String("region", "ap-northeast-1", "the AWS region where your Kinesis Stream is.")
 	iteratorType = flag.String("iterator-type", "TRIM_HORIZON", "iterator type. Choose from TRIM_HORIZON(default), AT_SEQUENCE_NUMBER, AT_TIMESTAMP or LATEST.")
-	maxItemSize  = flag.Int("max-item-size", 4096, "max byte size per item for printing.")
-	forever      = flag.Bool("f", true, "tailing kinesis stream forever or not (like: tail -f)")
-	limit        = flag.Int64("limit", 100, "limit records length of each GetRecords request.")
-	interval     = flag.Duration("interval", time.Second*3, "seconds for waiting next GetRecords request.")
+	interval     = flag.Duration("interval", 3*time.Second, "seconds for waiting next GetRecords request.")
 	startTime    = flag.String("start-time", "", "timestamp to start reading. only enable when iterator type is AT_TIMESTAMP. acceptable format is YYYY-MM-DDThh:mm:ss.sssTZD (RFC3339 format). For example, 2016-04-20T12:00:00+09:00 is acceptable.")
 )
-
-type Client struct {
-	*kinesis.Kinesis
-	maxItemSize int
-	Limit       *int64
-}
 
 func main() {
 	flag.Parse()
 
-	s := session.New(&aws.Config{Region: aws.String(*region)})
-	c := &Client{Kinesis: kinesis.New(s), maxItemSize: *maxItemSize, Limit: limit}
+	c := getKinesisClient()
 
 	var start time.Time
 	if *iteratorType == "AT_TIMESTAMP" && *startTime != "" {
 		t, err := time.Parse(time.RFC3339, *startTime)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "parse time failed. -start-time format should be RFC3339 format.: %s", err)
-			os.Exit(1)
+			fail(fmt.Sprintf("parse time failed. -start-time format should be RFC3339 format.: %s", err))
 		}
 		start = t
 	}
@@ -48,56 +37,69 @@ func main() {
 
 	streams, err := c.DescribeStream(&kinesis.DescribeStreamInput{StreamName: streamName})
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "cannot describe stream. please verify your stream is accecible.: %s", err)
+		fmt.Fprintf(os.Stderr, "Cannot describe stream. Please verify your stream is accessible.: %s", err)
 		os.Exit(1)
 	}
-	fmt.Printf("Your Stream information: %v\n", streams)
 
-	iteratorOutput, err := c.GetShardIterator(&kinesis.GetShardIteratorInput{
-		// take first shard.
-		ShardId:           streams.StreamDescription.Shards[0].ShardId,
-		ShardIteratorType: aws.String(*iteratorType),
-		StreamName:        streamName,
-		Timestamp:         aws.Time(start),
-	})
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "cannot get iterator: %s", err)
-		os.Exit(1)
-	}
-	fmt.Printf("%v\n", iteratorOutput)
+	records := make(chan string)
 
-	iter := iteratorOutput.ShardIterator
-	for {
-		if iter == nil && *forever == false {
-			fmt.Fprintf(os.Stderr, "all data in this stream is fetched. : %s")
-			os.Exit(0)
-		}
-		iter, err = c.fetch(iter)
+	for _, shard := range streams.StreamDescription.Shards {
+		iterator, err := getIterator(c, streamName, shard, &start)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "get records failed: %s", err)
-			os.Exit(1)
+			fail(fmt.Sprintf("Could not get iterator for shard %s: %v", *shard.ShardId, err))
 		}
-		time.Sleep(*interval)
+
+		go func(s *kinesis.Shard) {
+			iter := iterator.ShardIterator
+			for {
+				iter, err = fetchRecords(c, iter, records)
+				if err != nil {
+					fail(fmt.Sprintf("Failed reading from shard %s: %v\n", *s.ShardId, err))
+				}
+				time.Sleep(*interval)
+			}
+		}(shard)
+	}
+
+	for record := range records {
+		fmt.Println(record)
 	}
 }
 
-// iter fetch records.
-func (c *Client) fetch(shardIterator *string) (nextShardIterator *string, err error) {
-	records, err := c.GetRecords(&kinesis.GetRecordsInput{
-		ShardIterator: shardIterator,
-		Limit:         c.Limit,
+func getIterator(k *kinesis.Kinesis, stream *string, shard *kinesis.Shard, t *time.Time) (*kinesis.GetShardIteratorOutput, error) {
+	return k.GetShardIterator(&kinesis.GetShardIteratorInput{
+		ShardId:           shard.ShardId,
+		ShardIteratorType: aws.String(*iteratorType),
+		StreamName:        stream,
+		Timestamp:         t,
 	})
+}
+
+func getKinesisClient() *kinesis.Kinesis {
+	s, err := session.NewSession(&aws.Config{Region: aws.String(*region)})
+	if err != nil {
+		fail(fmt.Sprintf("Could not create AWS session: %v", err))
+	}
+
+	return kinesis.New(s)
+}
+
+func fail(msg string) {
+	fmt.Fprintln(os.Stderr, msg)
+	os.Exit(1)
+}
+
+// iter fetch records.
+func fetchRecords(k *kinesis.Kinesis, shardIterator *string, out chan string) (*string, error) {
+	records, err := k.GetRecords(&kinesis.GetRecordsInput{
+		ShardIterator: shardIterator,
+	})
+
 	if err != nil {
 		return nil, err
 	}
 	for _, r := range records.Records {
-		fmt.Printf("ApproximateArrivalTimestamp: %v\n", r.ApproximateArrivalTimestamp)
-		if len(r.Data) > c.maxItemSize {
-			fmt.Printf("Data: %s\n", r.Data[:c.maxItemSize-1])
-		} else {
-			fmt.Printf("Data: %s\n", r.Data[:])
-		}
-		fmt.Printf("SequenceNumber: %s\n", *r.SequenceNumber)
+		out <- string(r.Data)
 	}
 	return records.NextShardIterator, nil
 }
